87f47ce570bfa601fc201ed5cb2b9f953b94d4f2
gribyonkin <i.a.greebyonkin@yandex.ru>
2024-01-29 18:18:33 +0300

    2

--- py_main.py
+++ py_main.py
@@ -5,3 +5,9 @@ def fun1(hashMap,_files=None,_data=None):
   hashMap.put("toast","Мой первый коммит!!! ad92")
 
   return hashMap
+
+def get_prices(hashMap,_files=None,_data=None):
+
+  hashMap.put("toast","Мой первый коммит!!! ad92")
+
+  return hashMap
