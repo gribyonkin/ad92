e0981ae60bfbe7ee38e425b6f7ec54785d6dcff6
gribyonkin <i.a.greebyonkin@yandex.ru>
2024-01-29 18:36:41 +0300

    3

--- py_main.py
+++ py_main.py
@@ -11,3 +11,9 @@ def get_prices(hashMap,_files=None,_data=None):
   hashMap.put("toast","Мой первый коммит!!! ad92")
 
   return hashMap
+
+def after_scan(hashMap,_files=None,_data=None):
+
+  hashMap.put("showscreen","Найден товар")
+
+  return hashMap
