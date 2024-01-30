348145e799eee650ad3f7e91e7b2b0e939eceba4
gribyonkin <i.a.greebyonkin@yandex.ru>
2024-01-30 11:31:00 +0300

    8

--- py_main.py
+++ py_main.py
@@ -13,6 +13,9 @@ def get_prices(hashMap,_files=None,_data=None):
   return hashMap
 
 def after_scan(hashMap,_files=None,_data=None):
+
+  hashMap.put("article","oc90")
+  hashMap.put("brand","Mahle")
   hashMap.put("warname", hashMap.get("barcode"))
   hashMap.put("ShowScreen","Найден товар")
 
