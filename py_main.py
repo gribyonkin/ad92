8214129bde2c34a6e771325cb1db33e93ee7209e
gribyonkin <i.a.greebyonkin@yandex.ru>
2024-02-26 14:15:30 +0300

    iyt

--- py_main.py
+++ py_main.py
@@ -8,6 +8,139 @@ def fun1(hashMap,_files=None,_data=None):
 
   return hashMap
 
+def fun_cards(hashMap,_files=None,_data=None):
+
+  j = { "customcards":         {
+          "options":{
+            "search_enabled":True,
+            "save_position":True
+          },
+
+          "layout": {
+          "type": "LinearLayout",
+          "orientation": "vertical",
+          "height": "match_parent",
+          "width": "match_parent",
+          "weight": "0",
+          "Elements": [
+          {
+              "type": "LinearLayout",
+              "orientation": "horizontal",
+              "height": "wrap_content",
+              "width": "match_parent",
+              "weight": "0",
+              "Elements": [
+              {
+              "type": "Picture",
+              "show_by_condition": "",
+              "Value": "@pic1",
+              "NoRefresh": False,
+              "document_type": "",
+              "mask": "",
+              "Variable": "",
+              "TextSize": "16",
+              "TextColor": "#DB7093",
+              "TextBold": True,
+              "TextItalic": False,
+              "BackgroundColor": "",
+              "width": "match_parent",
+              "height": "wrap_content",
+              "weight": 2
+              },
+              {
+              "type": "LinearLayout",
+              "orientation": "vertical",
+              "height": "wrap_content",
+              "width": "match_parent",
+              "weight": "1",
+              "Elements": [
+              {
+                  "type": "TextView",
+                  "show_by_condition": "",
+                  "Value": "@string1",
+                  "NoRefresh": False,
+                  "document_type": "",
+                  "mask": "",
+                  "Variable": ""
+              },
+              {
+                  "type": "TextView",
+                  "show_by_condition": "",
+                  "Value": "@string2",
+                  "NoRefresh": False,
+                  "document_type": "",
+                  "mask": "",
+                  "Variable": ""
+              },
+              {
+                  "type": "TextView",
+                  "show_by_condition": "",
+                  "Value": "@string3",
+                  "NoRefresh": False,
+                  "document_type": "",
+                  "mask": "",
+                  "Variable": ""
+              }
+              ]
+              },
+              {
+              "type": "TextView",
+              "show_by_condition": "",
+              "Value": "@val",
+              "NoRefresh": False,
+              "document_type": "",
+              "mask": "",
+              "Variable": "",
+              "TextSize": "16",
+              "TextColor": "#DB7093",
+              "TextBold": True,
+              "TextItalic": False,
+              "BackgroundColor": "",
+              "width": "match_parent",
+              "height": "wrap_content",
+              "weight": 2
+              }
+              ]
+          },
+          {
+              "type": "TextView",
+              "show_by_condition": "",
+              "Value": "@descr",
+              "NoRefresh": False,
+              "document_type": "",
+              "mask": "",
+              "Variable": "",
+              "TextSize": "-1",
+              "TextColor": "#6F9393",
+              "TextBold": False,
+              "TextItalic": True,
+              "BackgroundColor": "",
+              "width": "wrap_content",
+              "height": "wrap_content",
+              "weight": 0
+          }
+          ]
+      }
+
+  }
+  }
+
+  j["customcards"]["cardsdata"]=[]
+  for i in range(0,2):
+      c =  {
+      "key": str(i),
+      "descr": "Pos. "+str(i),
+      "val": str(random.randint(10, 10000))+" руб.",
+      "string1": "Материнская плата ASUS ROG MAXIMUS Z690 APEX",
+      "string2": "Гнездо процессора LGA 1700",
+      "string3": "Частотная спецификация памяти 4800 МГц"
+      }
+    j["customcards"]["cardsdata"].append(c)
+
+  hashMap.put("cards",json.dumps(j,ensure_ascii=False).encode('utf8').decode())
+
+  return hashMap
+
 def get_prices(hashMap,_files=None,_data=None):
     
   art = hashMap.get("article")
