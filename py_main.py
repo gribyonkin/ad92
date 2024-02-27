a7037bca0323069b92439af4f370c9f6be48ae13
gribyonkin <i.a.greebyonkin@yandex.ru>
2024-02-27 05:24:05 +0300

    yvd

--- py_main.py
+++ py_main.py
@@ -3,6 +3,160 @@ import requests
 import json
 import random
 
+def fill_prices_card(hashMap,_files=None,_data=None):
+    
+    j = { "customcards":         {
+            "options":{
+              "search_enabled":True,
+              "save_position":True
+            },
+            "layout": {
+            "type": "LinearLayout",
+            "orientation": "vertical",
+            "height": "match_parent",
+            "width": "match_parent",
+            "weight": "0",
+            "Elements": [
+            {
+                "type": "LinearLayout",
+                "orientation": "horizontal",
+                "height": "wrap_content",
+                "width": "match_parent",
+                "weight": "0",
+                "Elements": [
+                {
+                "type": "CheckBox",
+                "Value": "@cb1",
+                "NoRefresh": False,
+                "document_type": "",
+                "mask": "",
+                "Variable": "cb1",
+                "BackgroundColor": "#DB7093",
+                "width": "match_parent",
+                "height": "wrap_content",
+                "weight": 2
+                },
+                {
+                "type": "LinearLayout",
+                "orientation": "vertical",
+                "height": "wrap_content",
+                "width": "match_parent",
+                "weight": "1",
+                "Elements": [
+                {
+                    "type": "TextView",
+                    "show_by_condition": "",
+                    "Value": "@price_str",
+                    "NoRefresh": False,
+                    "document_type": "",
+                    "mask": "",
+                    "Variable": ""
+                },
+                {
+                    "type": "TextView",
+                    "show_by_condition": "",
+                    "Value": "@quant_str",
+                    "NoRefresh": False,
+                    "document_type": "",
+                    "mask": "",
+                    "Variable": ""
+                },
+                {
+                    "type": "Button",
+                    "show_by_condition": "",
+                    "Value": "#f290",
+                    "TextColor": "#DB7093",
+                    "Variable": "btn_tst1",
+                    "NoRefresh": False,
+                    "document_type": "",
+                    "mask": ""
+                    
+                },
+                {
+                    "type": "Button",
+                    "show_by_condition": "",
+                    "Value": "#f469",
+                    "TextColor": "#DB7093",
+                    "Variable": "btn_tst2",
+                    "NoRefresh": False,
+                    "document_type": "",
+                    "mask": ""
+                    
+                }
+                ]
+                },
+                {
+                "type": "TextView",
+                "show_by_condition": "",
+                "Value": "@val",
+                "NoRefresh": False,
+                "document_type": "",
+                "mask": "",
+                "Variable": "",
+                "TextSize": "10",
+                "TextColor": "#DB7093",
+                "TextBold": True,
+                "TextItalic": False,
+                "BackgroundColor": "",
+                "width": "match_parent",
+                "height": "wrap_content",
+                "weight": 2
+                },
+               {
+                "type": "PopupMenuButton",
+                "show_by_condition": "",
+                "Value": "Удалить;Проверить",
+                "NoRefresh": False,
+                "document_type": "",
+                "mask": "",
+                "Variable": "menu_delete"
+                
+                }
+                ]
+            },
+            {
+                "type": "TextView",
+                "show_by_condition": "",
+                "Value": "@descr",
+                "NoRefresh": False,
+                "document_type": "",
+                "mask": "",
+                "Variable": "",
+                "TextSize": "-1",
+                "TextColor": "#6F9393",
+                "TextBold": False,
+                "TextItalic": True,
+                "BackgroundColor": "",
+                "width": "wrap_content",
+                "height": "wrap_content",
+                "weight": 0
+            }
+            ]
+        }
+
+    }
+    }
+   
+    j["customcards"]["cardsdata"]=[]
+    for i in range(0,5):
+      c =  {
+        "key": str(i),       
+        "descr": "Pos. "+str(i),
+        "val": str(random.randint(10, 10000))+" руб.",
+        "string1": "Материнская плата ASUS ROG MAXIMUS Z690 APEX",
+        "string2": "Гнездо процессора LGA 1700",
+        "string3": "Частотная спецификация памяти 4800 МГц"
+      }
+      
+      j["customcards"]["cardsdata"].append(c)
+
+    #if not hashMap.containsKey("cards"):
+    
+    hashMap.put("cards",json.dumps(j,ensure_ascii=False).encode('utf8').decode())
+    
+    return hashMap
+
+
 def fun1(hashMap,_files=None,_data=None):
     
     j = { "customcards":         {
