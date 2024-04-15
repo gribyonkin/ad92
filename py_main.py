b6151da5b53a551378209231f7822396815da2b9
gribyonkin <i.a.greebyonkin@yandex.ru>
2024-04-15 18:19:42 +0300

    zxczg

--- py_main.py
+++ py_main.py
@@ -37,7 +37,96 @@ def choice_profile(hashMap,_files=None,_data=None):
   response = requests.post('https://avtodrug92.freno.ru/ApiRequest/getOneOffer', data=param_tuples)
 
   json_data = response.json()
+  j = { "customcards":         {
+          "options":{
+            "search_enabled":True,
+            "save_position":True
+          },
+          "layout": {
+          "type": "LinearLayout",
+          "orientation": "vertical",
+          "height": "match_parent",
+          "width": "match_parent",
+          "weight": "0",
+          "Elements": [
+          {
+              "type": "LinearLayout",
+              "orientation": "horizontal",
+              "height": "wrap_content",
+              "width": "match_parent",
+              "weight": "0",
+              "Elements": [
+              {
+              "type": "LinearLayout",
+              "orientation": "vertical",
+              "height": "wrap_content",
+              "width": "match_parent",
+              "weight": "1",
+              "Elements": [
+              {
+                  "type": "TextView",
+                  "show_by_condition": "",
+                  "Value": "@price_str",
+                  "NoRefresh": False,
+                  "document_type": "",
+                  "mask": "",
+                  "Variable": ""
+              }              
+              ]
+              },
+              {
+              "type": "TextView",
+              "show_by_condition": "",
+              "Value": "@val",
+              "NoRefresh": False,
+              "document_type": "",
+              "mask": "",
+              "Variable": "",
+              "TextSize": "20",
+              "TextColor": "#DB7093",
+              "TextBold": True,
+              "TextItalic": False,
+              "BackgroundColor": "",
+              "width": "match_parent",
+              "height": "wrap_content",
+              "weight": 2
+              },
+              {
+                "type": "Button",
+                "show_by_condition": "",
+                "Value": "#f290",
+                "TextColor": "#DB7093",
+                "Variable": "btn_tst1",
+                "NoRefresh": False,
+                "document_type": "",
+                "mask": ""                  
+              }              
+              ]
+          },
+          {
+              "type": "TextView",
+              "show_by_condition": "",
+              "Value": "@descr",
+              "NoRefresh": False,
+              "document_type": "",
+              "mask": "",
+              "Variable": "",
+              "TextSize": "-1",
+              "TextColor": "#6F9393",
+              "TextBold": False,
+              "TextItalic": True,
+              "BackgroundColor": "",
+              "width": "wrap_content",
+              "height": "wrap_content",
+              "weight": 0
+          }
+          ]
+      }
 
+  }
+  }
+  
+  j["customcards"]["cardsdata"]=[]
   hashMap.put("toast", "Привет!!")
   
   return hashMap
